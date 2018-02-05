d686497e71a8fbdc952f10fdb7fa15248f4be8c4
Iverson <ilp_1995@hotmail.com>
2018-02-04 23:17:40 -0300

    final version

--- text-detection.py
+++ text-detection.py
@@ -12,35 +12,61 @@ from time import time
 from scipy.spatial import KDTree
 from matplotlib import pyplot as plt
 
-#paramenters that can to influence the result
-CANNY_THRESHOLD_MIN = 250
-CANNY_THRESHOLD_MAX = 400
+#paramenters that can to influence the result of edge
+USE_THRESHOLD_MANUAL = False
+CANNY_THRESHOLD_MIN = 200
+CANNY_THRESHOLD_MAX = 300
+#paramenters to morphology
 WITH_HIST_EQU = False
-WITH_MORPH_DIL = True
-MORPH_WINDOW = (7, 7)
+WITH_MORPH = False
+WITH_MORPH_OPEN = False
+WITH_MORPH_CLOSE = True
+MORPH_WINDOW = (9, 9)
 INTERATIONS_MORPH = 1
+#paramenter to swt
 MAX_RAY_LEN = 100
 MAX_ANGL_DIFF = math.pi/2
 
+# 1.pre-processsing: pre_precossing, auto_canny, edge_detection, gradient_detection
 def pre_processing(original_img):
     """
     improve the image to a best edge detection
-    """   
+    """
     img_gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     if WITH_HIST_EQU:
         return cv2.equalizeHist(img_gray)
     else:
         return img_gray     
 
+def auto_canny(image, sigma=0.33):
+    if USE_THRESHOLD_MANUAL:
+        edged_manual = cv2.Canny(image, CANNY_THRESHOLD_MIN, CANNY_THRESHOLD_MAX)
+        return edged_manual
+ 
+	# apply automatic Canny edge detection
+    
+    v = np.median(image)
+
+    lower = int(max(0, (1.0 - sigma) * v))
+    upper = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(image, lower, upper)
+
+    return edged
+
 def edge_detection(img_preprocessed):
     """
     detects edges of an image
     """
-    img_edges = cv2.Canny(img_preprocessed, CANNY_THRESHOLD_MIN, CANNY_THRESHOLD_MAX)   
-    if WITH_MORPH_DIL:
+    img_edges = auto_canny(img_preprocessed)
+    if WITH_MORPH:
         kernel = np.ones(MORPH_WINDOW, np.uint8)
-        dilated = cv2.dilate(img_edges, kernel, INTERATIONS_MORPH)
-        return dilated
+        if WITH_MORPH_CLOSE:           
+            closing = cv2.morphologyEx(img_edges, cv2.MORPH_CLOSE, kernel)
+            return closing
+        if WITH_MORPH_OPEN:
+            opening = cv2.morphologyEx(img_edges, cv2.MORPH_OPEN, kernel)
+            return opening
+
     else:
         return img_edges
 
@@ -54,6 +80,7 @@ def gradient_detection(img_preprocessed):
 
     return sobelx64f, sobely64f, theta
 
+# 2.swt algorithm
 def stroke_width_transform(edges, sobelx64f, sobely64f, theta, verbose=0):
     """
     find the stroke of edges
@@ -114,6 +141,7 @@ def stroke_width_transform(edges, sobelx64f, sobely64f, theta, verbose=0):
 
     return swt
 
+# 3.componenets extraction: connected_components, get_letters and get_words
 def connected_components(swt):
     """
     detects the connected components from swt result image, verifying each stroke width
@@ -207,16 +235,11 @@ def connected_components(swt):
                             connected_neighbors = make_set(label_n)
 
                 if not connected_neighbors:
-                    # We don't see any connections to North/West
                     trees[next_label] = (make_set(next_label))
                     label_map[y, x] = next_label
                     next_label += 1
                 else:
-                    # We have at least one connection to North/West
                     label_map[y, x] = min(neighborvals)
-                    # For each neighbor, make note that their respective connected_neighbors are connected
-                    # for label in connected_neighbors.
-                    # @TODO: do I need to loop at all neighbor trees?
                     trees[connected_neighbors.value] = union(trees[connected_neighbors.value], connected_neighbors)
 
     # Second pass. re-base all labeling with representative label for each connected tree
@@ -255,7 +278,7 @@ def get_letters(swt, comp):
 
         diameter = math.sqrt(width**2 + height**2)
         median_swt = np.median(swt[(c['y'], c['x'])])
-        if diameter / median_swt > 15:  # TODO: this threshold can be improved?
+        if diameter / median_swt > 15:
             continue
 
         if width / img_w > 0.4 or height / img_h > 0.4:
@@ -272,7 +295,7 @@ def get_letters(swt, comp):
 
     return np.asarray(letters)
 
-def get_words(letters): # swts, heights, widths, topleft_pts, images):
+def get_words(letters):
     # Index-pairs of letter with similar median stroke widths and similar heights
     # We use log2 for linear distance comparison in KDTree
     # (i.e. if log2(x) - log2(y) > 1, we know that x > 2*y)
@@ -292,18 +315,15 @@ def get_words(letters): # swts, heights, widths, topleft_pts, images):
             angle += math.pi if angle < 0 else 0
             pairs.append([ix_letr1, ix_letr2, angle])
     pairs = np.asarray(pairs)
-
+    
     # Pairs of letter-pairs with a similar angle (direction of text)
     a_ix_pair_pairs = KDTree(pairs[:, 2:3]).query_pairs(math.pi / 12)
 
     chains = []
     for ix_pair_a, ix_pair_b in a_ix_pair_pairs:
-        # Letter pairs [a] & [b] have a similar angle and each pair consists of
-        # letter [1] & [2] which meet the similarity-requirements.
         pair_a_letr1, pair_a_letr2 = int(pairs[ix_pair_a, 0]), int(pairs[ix_pair_a, 1])
         pair_b_letr1, pair_b_letr2 = int(pairs[ix_pair_b, 0]), int(pairs[ix_pair_b, 1])
 
-        # TODO: not correct?
         added = False
         for c in chains:
             if pair_a_letr1 in c:
@@ -326,16 +346,12 @@ def get_words(letters): # swts, heights, widths, topleft_pts, images):
             chains.append({pair_b_letr1, pair_b_letr2})
     chains = np.asarray(chains)
 
-    # List of sets of letters with possibly many duplicates
-    # return chains
-    # Single list of unique letters
-    # return np.unique([int(ix) for chain in chains if len(chain) >= 3 for ix in chain])
-
     vecfunc = np.vectorize(len)
     chains = chains[vecfunc(chains) > 3]
     _, uniq_ix = np.unique(chains.astype(str), return_index=True)
     return chains[uniq_ix]
 
+# 4.get the mask to apply in original image
 def draw_mask(original_img, letters, words):
     words_positions = []
     pt1 = letters[:, 3:5].astype(int)
@@ -349,13 +365,139 @@ def draw_mask(original_img, letters, words):
         words_positions = np.column_stack([pt1, pt2])
     else:
         words_positions = np.array([[pt1[list(w)][:, 0].min(), pt1[list(w)][:, 1].min(), pt2[list(w)][:, 0].max(), pt2[list(w)][:, 1].max()] for w in words])
-
-    final_img = original_img
+    
+    final_img = original_img   
+    print(compare_with_dataset(words_positions))
+    #write_file(compare_with_dataset(words_positions), "teste.jpg")
     for _, [pt1y, pt1x, pt2y, pt2x] in enumerate(words_positions):
         cv2.rectangle(final_img, (pt1x, pt1y), (pt2x, pt2y), (0, 255, 0), 4)
+    
+    #cv2.rectangle(final_img, (171, 596), (1254, 951), (0, 0, 255), 2)
+
+    #cv2.line(final_img, (pt1x, pt1y), (pt2x, pt2y), (0, 0, 255), thickness=3, lineType=8)
 
     return final_img
 
+def draw_mask_letters(original_img, letters, words=None):
+    letter_positions = []
+    pt1 = letters[:, 3:5].astype(int)
+
+    # south-east point [pt2] = (north + height, west + width)
+    pt2_y = letters[:, 3] + letters[:, 1]
+    pt2_x = letters[:, 4] + letters[:, 2]
+    pt2 = np.vstack([pt2_y, pt2_x]).T.astype(int)
+    
+    letter_positions = np.column_stack([pt1, pt2])
+    
+    final_img = original_img
+    for _, [pt1y, pt1x, pt2y, pt2x] in enumerate(letter_positions):
+        cv2.rectangle(final_img, (pt1x, pt1y), (pt2x, pt2y), (255, 0, 0), 2)
+
+    return final_img
+
+def rotate_image(image, angle):
+    """
+    Rotates an OpenCV 2 / NumPy image about it's centre by the given angle
+    (in radians). The returned image will be large enough to hold the entire
+    new image, with a black background
+    """
+
+    # Get the image size
+    # No that's not an error - NumPy stores image matricies backwards
+    image_size = (image.shape[1], image.shape[0])
+    image_center = tuple(np.array(image_size) / 2)
+
+    # Convert the OpenCV 3x2 rotation matrix to 3x3
+    rot_mat = np.vstack(
+        [cv2.getRotationMatrix2D(image_center, angle, 1.0), [0, 0, 1]]
+    )
+
+    rot_mat_notranslate = np.matrix(rot_mat[0:2, 0:2])
+
+    # Shorthand for below calcs
+    image_w2 = image_size[0] * 0.5
+    image_h2 = image_size[1] * 0.5
+
+    # Obtain the rotated coordinates of the image corners
+    rotated_coords = [
+        (np.array([-image_w2,  image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([ image_w2,  image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([-image_w2, -image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([ image_w2, -image_h2]) * rot_mat_notranslate).A[0]
+    ]
+
+    # Find the size of the new image
+    x_coords = [pt[0] for pt in rotated_coords]
+    x_pos = [x for x in x_coords if x > 0]
+    x_neg = [x for x in x_coords if x < 0]
+
+    y_coords = [pt[1] for pt in rotated_coords]
+    y_pos = [y for y in y_coords if y > 0]
+    y_neg = [y for y in y_coords if y < 0]
+
+    right_bound = max(x_pos)
+    left_bound = min(x_neg)
+    top_bound = max(y_pos)
+    bot_bound = min(y_neg)
+
+    new_w = int(abs(right_bound - left_bound))
+    new_h = int(abs(top_bound - bot_bound))
+
+    # We require a translation matrix to keep the image centred
+    trans_mat = np.matrix([
+        [1, 0, int(new_w * 0.5 - image_w2)],
+        [0, 1, int(new_h * 0.5 - image_h2)],
+        [0, 0, 1]
+    ])
+
+    # Compute the tranform for the combined rotation and translation
+    affine_mat = (np.matrix(trans_mat) * np.matrix(rot_mat))[0:2, :]
+
+    # Apply the transform
+    result = cv2.warpAffine(
+        image,
+        affine_mat,
+        (new_w, new_h),
+        flags=cv2.INTER_LINEAR
+    )
+
+    return result
+
+def compare_with_dataset(words_points):
+    values = []
+    for _, [pt1y, pt1x, pt2y, pt2x] in enumerate(words_points):
+        word = [pt1x, pt1y, pt2x-pt1x, pt2y-pt1y]
+        values.append(word)
+    
+    return values
+
+def write_file(values, filename):
+    file = open(''.join(["output/", filename[:-4]]), "w")
+    for _, [x, y, w, h] in enumerate(values):
+        out = ''.join([str(x), ' ', str(y), ' ', str(w), ' ', str(h)])
+        file.write(out)
+    file.close()
+
+#main to apply algorithm in all imagens
+def main_loop():
+    for file in os.listdir("MSRA-TD500/test/"):
+        if file.endswith(".JPG"):
+            filepath = ''.join(["MSRA-TD500/test/", file])
+            print(file)
+            original_img = cv2.imread(filepath)
+            img_preprocessed = pre_processing(original_img)
+            edges = edge_detection(img_preprocessed)
+            cv2.imwrite(''.join(["output/", file[:-4], "_edges.jpg"]), edges)
+            sobelx, sobely, theta = gradient_detection(img_preprocessed)
+            swt_result = stroke_width_transform(edges, sobelx, sobely, theta)
+            cv2.imwrite(''.join(["output/", file[:-4], "_swt_result.jpg"]), swt_result)
+            components = connected_components(swt_result)
+            letters = get_letters(swt_result, components)
+            words = get_words(letters)
+            result_img = draw_mask(original_img, letters, words)
+            cv2.imwrite(''.join(["output/", file[:-4], "_result.jpg"]), result_img)
+
+#main to apply algorithm in an image
 def main():
     """
     run the program to detect text in images
@@ -364,13 +506,18 @@ def main():
     original_img = cv2.imread(filepath)
     img_preprocessed = pre_processing(original_img)
     edges = edge_detection(img_preprocessed)
+    cv2.imwrite("output/edges.jpg", edges)
     sobelx, sobely, theta = gradient_detection(img_preprocessed)
     swt_result = stroke_width_transform(edges, sobelx, sobely, theta)
+    cv2.imwrite("output/swt_result.jpg", swt_result)
     components = connected_components(swt_result)
     letters = get_letters(swt_result, components)
     words = get_words(letters)
     result_img = draw_mask(original_img, letters, words)
+    #result_img = draw_mask_letters(result_img, letters)
     cv2.imwrite("output/result.jpg", result_img)
-
 #start the program
-main()
+if sys.argv.__len__() > 1:
+    main()
+else:
+    main_loop()
